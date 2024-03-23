d03351653c467df2a74384a0241e1209268cf97e
Omar Crosby <omar.crosby@gmail.com>
2024-03-23 01:01:13 -0400

    chore: transition to CMake

--- scripts/install_gtest.sh
+++ scripts/install_gtest.sh
@@ -4,12 +4,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Ubuntu
     sudo apt-get update
     sudo apt-get install -y libgtest-dev cmake
-    pushd /usr/src/gtest
+    cd /usr/src/gtest
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OS
     brew install cmake
     brew install googletest
-    pushd "$(brew --prefix googletest)/src"
+    cd "$(brew --prefix googletest)/src"
 fi
 
 # Build the sources
@@ -19,5 +19,3 @@ sudo make
 # Copy the static libraries to /usr/lib
 sudo cp *.a /usr/lib
 
-# Clean up
-popd
